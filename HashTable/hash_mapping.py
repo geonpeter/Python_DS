99253e0d880f39fe1acd23ac9ef5eb7c076d658c
Geon Peter <igeonpeter@gmail.com>
2024-11-14 11:23:14 +0100

    Add Hash Mapping

---
+++ HashTable/hash_mapping.py
@@ -0,0 +1,20 @@
+class HashTable:
+    def __init__(self):
+        self.Max = 100
+        self.arr = [ None for _ in range(self.Max)]
+        
+    def get_hash(self,k):
+        h =0
+        for c in k:
+            h+=ord(c)  # return the ascii value for each character in the key and adding to h
+        return h% self.Max # preparing an index value from hash function
+    def __setitem__(self,k,v):
+        h = self.get_hash(k)
+        self.arr[h]=v
+    def __getitem__(self,k):
+        h = self.get_hash(k)
+        return self.arr[h]
+    
+    def __delitem__(self,k):
+        h = self.get_hash(k)
+        self.arr[h]=None
